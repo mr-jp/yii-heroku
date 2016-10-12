fd16583b06ca9cc1d3e51f251f0509d3729fa110
Jason <jasonphoon@gmail.com>
2016-10-12 15:08:20 +0800

    Removed dev requirement for toolbar

--- environments/heroku/backend/config/main-local.php
+++ environments/heroku/backend/config/main-local.php
@@ -9,12 +9,10 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-}
+// configuration adjustments for 'dev' environment
+$config['bootstrap'][] = 'debug';
+$config['modules']['debug'] = [
+    'class' => 'yii\debug\Module',
+];
 
 return $config;
